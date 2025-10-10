3c3e37e342c74e3c3114767ce40f0a3c96e6e16f
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-10-11 00:40:06 +0700

    Вынес файл безопасности и фильтры в общий модуль. Настроил регистрацию клиентов в eureka сервере

--- user-service/src/main/java/com/example/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/example/user_service/UserServiceApplication.java
@@ -1,9 +1,28 @@
 package com.example.user_service;
 
+import com.example.common_module.config.ContentTypeConfig;
+import com.example.common_module.config.SecurityConfig;
+import com.example.common_module.filters.ContentTypeFilter;
+import com.example.common_module.filters.JwtAuthenticationFilter;
+import com.example.common_module.handler.AccessDeniedHandlerImpl;
+import com.example.common_module.handler.AuthenticationEntryPointImpl;
+import com.example.common_module.handler.CustomLogoutHandler;
+import com.example.common_module.jwt.TokenService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({
+		SecurityConfig.class,
+		TokenService.class,
+		ContentTypeFilter.class,
+		ContentTypeConfig.class,
+		JwtAuthenticationFilter.class,
+		CustomLogoutHandler.class,
+		AuthenticationEntryPointImpl.class,
+		AccessDeniedHandlerImpl.class
+})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
