f1d4f0cf93617cb5646f518a7acd51c2ac56673b
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-10-13 18:24:52 +0700

    убрал сервис заказов

--- orders-service/src/main/java/com/example/orders_service/repository/RatingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.orders_service.repository;
-
-import com.example.orders_service.domain.entity.Rating;
-import com.example.orders_service.domain.entity.RatingPK;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RatingRepository extends JpaRepository<Rating, RatingPK> {
-}
--- orders-service/src/main/resources/db.migration/V1__init_orders.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE order_rating (
-    user_id UUID NOT NULL,
-    product_id UUID NOT NULL,
-    rating DOUBLE PRECISION NOT NULL,
-    PRIMARY KEY (user_id, product_id)
-);
