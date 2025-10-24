44ff48622586eaad6f2d6c0db9e43de890fbe5f4
UDOUNO <jueh1wghbdtn.com@yandex.ru>
2025-10-24 22:02:18 +0700

    Fixed ManyToMany relation for order and meal

--- admin-service/src/main/java/orderservice/data/Reservation.java
+++ admin-service/src/main/java/orderservice/data/Reservation.java
@@ -32,7 +32,7 @@ public class Reservation {
     private LocalDate date;
     private Status status;
     private PayWay payWay;
-    @OneToMany(cascade = CascadeType.ALL)
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     private List<Meal> meals;
 
     @PrePersist
