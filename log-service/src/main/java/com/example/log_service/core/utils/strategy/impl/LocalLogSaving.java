197c6862dc368a4dd6e1e5b85c7fb582e18ad1a0
Aymir Sunchugashev <as4n.tsu@gmail.com>
2026-02-10 17:01:22 +0700

    Fix bug with saving log files

--- log-service/src/main/java/com/example/log_service/core/utils/strategy/impl/LocalLogSaving.java
+++ log-service/src/main/java/com/example/log_service/core/utils/strategy/impl/LocalLogSaving.java
@@ -5,6 +5,7 @@ import com.example.log_service.api.dto.LogFrontendRequest;
 import com.example.log_service.core.service.interfaces.FileStorageService;
 import com.example.log_service.core.utils.strategy.interfaces.LogSavingStrategy;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedWriter;
